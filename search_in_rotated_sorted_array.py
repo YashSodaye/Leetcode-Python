d644cb77273b23c44b7ef8105686fa39f9045a6d
Yash Sodaye <yashsodaye13@gmail.com>
2026-04-10 23:59:16 +0530

    10 April 2026

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -36,3 +36,18 @@ def search(nums, target):
         if nums[mid] >= nums[left]:
             if nums[left] <= target < nums[mid]:
                 right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else: 
+                right = mid - 1
+    
+    return -1
+
+print(search([4,5,6,7,0,1,2], 0))
+
+print(search([4,5,6,7,0,1,2], 3))
+
+print(search([1], 0))
