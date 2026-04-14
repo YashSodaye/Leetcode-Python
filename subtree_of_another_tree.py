0dc1d9f1f577b43f9545b4939b2096be3489e840
Yash Sodaye <yashsodaye13@gmail.com>
2026-04-14 23:40:43 +0530

    14 April 2026

---
+++ subtree_of_another_tree.py
@@ -0,0 +1,91 @@
+'''
+572. Subtree of Another Tree
+
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+Example 1:
+
+
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+Example 2:
+
+
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+ 
+'''
+
+from typing import Optional, List
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if subRoot is None:
+            return True
+        if root is None:
+            return False
+        
+        if self.isSameTree(root, subRoot):
+            return True
+        
+        return (self.isSubtree(root.left, subRoot) or 
+                self.isSubtree(root.right, subRoot))
+
+    def isSameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if root is None and subRoot is None:
+            return True
+        
+        if root is None or subRoot is None or root.val != subRoot.val:
+            return False
+
+        return (self.isSameTree(root.left, subRoot.left) and
+                self.isSameTree(root.right, subRoot.right))
+
+
+# Helper function to build tree from level-order list
+def build_tree(values: List[Optional[int]]) -> Optional[TreeNode]:
+    if not values:
+        return None
+    
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+    
+    while queue and i < len(values):
+        node = queue.popleft()
+        
+        if values[i] is not None:
+            node.left = TreeNode(values[i])
+            queue.append(node.left)
+        i += 1
+        
+        if i < len(values) and values[i] is not None:
+            node.right = TreeNode(values[i])
+            queue.append(node.right)
+        i += 1
+    
+    return root
+
+
+# ----------------- TEST CASES -----------------
+if __name__ == "__main__":
+    sol = Solution()
+    
+    # Example 1
+    root1 = build_tree([3,4,5,1,2])
+    subRoot1 = build_tree([4,1,2])
+    print("Example 1 Output:", sol.isSubtree(root1, subRoot1))  # True
+    
+    # Example 2
+    root2 = build_tree([3,4,5,1,2,None,None,None,None,0])
+    subRoot2 = build_tree([4,1,2])
+    print("Example 2 Output:", sol.isSubtree(root2, subRoot2))  # False
