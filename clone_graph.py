6d25c38449c56c8e2b122da07b841b0607c31ca4
Yash Sodaye <yashsodaye13@gmail.com>
2026-06-29 22:12:09 +0530

    29 June 2026

--- clone_graph.py
+++ clone_graph.py
@@ -140,7 +140,3 @@ cloned_graph = solution.cloneGraph(node1)
 print("\nCloned Graph:")
 print_graph(cloned_graph)
 
-print("Well Done")
-
-print("My finger is swollen, recovering day 17.")
-print("Hola")
