c3c5d0c83107a9ec93f69de52b3b1348c970af7d
Yash Sodaye <yashsodaye13@gmail.com>
2026-07-02 23:40:48 +0530

    02 July 2026

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -44,3 +44,4 @@ def contains_duplicate(nums):
 print(contains_duplicate([1,2,3,1]))
 print(contains_duplicate([1,2,3,4]))
 print(contains_duplicate([1,1,1,3,3,4,3,2,4,2]))
+print(contains_duplicate([1,9,8,7,6,5,4,5,6]))
